a7844eaa3cfd31532f5bed7d5028b65c27a394ce
Mudit Pandey <163019652+Mudit-2805@users.noreply.github.com>
2024-04-21 03:07:39 +0530

    Update Fibonnaci.py

--- Python/Fibonnaci.py
+++ Python/Fibonnaci.py
@@ -1,1 +1,17 @@
+n = int(input("Enter the nth number: "))
+a, b = 0, 1
 
+def fib(n, a, b):
+    
+    if n == 1:
+        print(0)
+    elif n > 1:
+        print(0, 1, end = " ")
+        for i in range(n-2):
+            j = a+b
+            print(j,end = " ")
+            a = b
+            b = j    
+            i += 1
+
+fib(n, a, b)
