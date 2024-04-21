0673276124f6d9472468e1e31537d18c7758f9cf
Mudit Pandey <163019652+Mudit-2805@users.noreply.github.com>
2024-04-22 04:28:58 +0530

    Update Binary_Search.py

--- Python/Binary_Search.py
+++ Python/Binary_Search.py
@@ -1,18 +1,18 @@
 L = [1,3,5,7,9]
 
-def Binary_Search(L, l, h, x):
+def Binary_Search(List, low, high, x):
     
-    if l > h:
+    if low > high:
         print("Element does not exist.")
         return False
     
-    mid = (l+h)//2
+    mid = (low+high)//2
     
-    if x == L[mid]:
+    if x == List[mid]:
         print("Element exists.")
-    elif x > L[mid]:
-        Binary_Search(L, mid+1, h, x)
+    elif x > List[mid]:
+        Binary_Search(L, mid+1, high, x)
     else:
-        Binary_Search(L, l, mid-1, x)
+        Binary_Search(L, low, mid-1, x)
         
 Binary_Search(L, 0, 5, 7)
