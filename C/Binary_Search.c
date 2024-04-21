bfaf0d48f2bb126634e389bba39f98ee9eeab4ed
Mudit Pandey <163019652+Mudit-2805@users.noreply.github.com>
2024-04-22 04:04:53 +0530

    Create Binary_Search.c

---
+++ C/Binary_Search.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int Binary_Search(int a[], int l, int h, int x){
+
+    if(l > h){ 
+        printf("It does not exist");
+        return 0;
+    }
+
+    int mid = (l + h)/2;
+
+    if(x == a[mid]){
+        printf("It exists");
+    } else if(x > a[mid]){
+        Binary_Search(a, mid+1, h, x);
+    } else if(x < a[mid]){
+        Binary_Search(a, l, mid-1, x);
+    }
+
+    return 0;
+}
+
+int main(){
+
+    int a[] = {1, 3, 5, 7, 9};
+
+    Binary_Search(a, 0, 5, 4);
+
+    return 0;
+}
